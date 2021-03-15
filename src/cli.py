5d319deaa5f45a431cc0a62a00f2db91f9348248
Dinko Pehar <pehar.dinko@nsoft.com>
2021-03-15 10:56:12 +0100

    Modify URL type from str to URLParserType

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,7 @@
 import argparse
 from http.cookies import SimpleCookie
 
+from utils import URLParserType
 from constants import EPILOG
 
 parser = argparse.ArgumentParser(description="Detect and exploit SQL injection flaws",
@@ -8,30 +9,29 @@ parser = argparse.ArgumentParser(description="Detect and exploit SQL injection f
                                  formatter_class=argparse.RawDescriptionHelpFormatter)
 
 # Required arguments.
-required_args = parser.add_argument_group(title="Required arguments:")
+required_args = parser.add_argument_group(title="Required arguments")
 required_args.add_argument('-u','--url',
-                           type=str, required=True,
+                           type=URLParserType, required=True,
                            help='Target URL (e.g. "http://www.site.com/vuln.php?id=1")'
                            )
 
 # Optional groups.
 
 # Request group.
-request_args = parser.add_argument_group(title="Request",
+request_args = parser.add_argument_group(title="Request arguments",
                                          description="Additional arguments when sending request.")
-# TODO: Add and modify this.
-request_args.add_argument('-H', '--header',
-                          nargs="*", action="append",
-                          help='Extra headers (e.g. "X-API-KEY: XXX")')
 request_args.add_argument('--method',
                           type=str, default="GET", choices=["GET", "POST", "PUT", "DELETE", "PATCH"],
                           help='Use different HTTP method (e.g. POST)')
+request_args.add_argument('--timeout',
+                          type=int, default=30,
+                          help='Seconds to wait before timeout connection (default 30s)')
+request_args.add_argument('-H', '--header',
+                          nargs="*", action="append",
+                          help='Extra headers (e.g. "X-API-KEY: XXX")')
 request_args.add_argument('--cookies',
                           type=SimpleCookie,
                           help='HTTP Cookies header value (e.g. "PHPSESSID=a8d127e;sec=low")')
-request_args.add_argument('--timeout',
-                          type=str, default=30,
-                          help='Seconds to wait before timeout connection (default 30)')
 request_args.add_argument('-a', '--auth',
                           type=str,
                           help='Pass a username:password pair for Basic Auth.')
