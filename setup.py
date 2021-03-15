159e4ac32cc1be1dd9c6521aef662874ef7b15ac
Dinko Pehar <pehar.dinko@nsoft.com>
2021-03-15 14:46:22 +0100

    Create setup.py installer

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+import nlsqli
+
+install_requires = [
+    'requests>=2.24',
+    'rich>=9.13.0'
+]
+
+setup(
+    name='nlsqli',
+    version=nlsqli.__version__,
+    description=nlsqli.__doc__.strip(),
+    author=nlsqli.__author__,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'nlsqli = nlsqli.__main__:main',
+        ],
+    },
+    python_requires='>=3.7',
+    install_requires=install_requires
+)
